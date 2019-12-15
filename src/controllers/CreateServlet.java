6fa5c78eed6dcae9aff1e4b116f340d590f8a8e4
yasu <yasu.git.33@gmail.com>
2019-12-15 20:07:45 +0900

    Add CreateServlet.java

---
+++ src/controllers/CreateServlet.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class CreateServlet
+ */
+@WebServlet("/create")
+public class CreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        //_tokenがnullではない時と_tokenとせっしよんのidが同じだった時
+        if(_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em= DBUtil.createEntityManager();
+
+            Task t = new Task();
+
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            //現在日時の応報を持つ日付型のオブジェクトを取得
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setCreated_at(currentTime);
+            t.setUpdated_at(currentTime);
+
+            //Taskクラスのオブジェクトをデーターベースに登録する処理
+            em.getTransaction().begin();
+            em.persist(t);
+            em.getTransaction().commit();
+            em.close();
+
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+
+}
