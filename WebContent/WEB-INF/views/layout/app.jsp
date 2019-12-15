1cd5f056e903001cc2ad36bff8abe75784e02636
yasu <yasu.git.33@gmail.com>
2019-12-15 19:32:42 +0900

    Modify Index/NewServlet

---
+++ WebContent/WEB-INF/views/layout/app.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+    <div id="wrapper">
+        <div id="header">
+            <h1>タスクリスト管理 アプリケーション</h1>
+        </div>
+        <div id="content">${param.content}</div>
+        <div id="footer">&copy; by y.y</div>
+    </div>
+
+</body>
+</html>
--- src/models/Task.java
+++ src/models/Task.java
@@ -1,30 +1,40 @@
 package models;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name="getAllTasks",
+            query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "tasks")
-public class Tasks {
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "create_at")
-    private Timestamp create_at;
+    @Column(name = "created_at")
+    private Timestamp created_at;
 
-    @Column(name = "update_at")
-    private Timestamp update_at;
+    @Column(name = "updated_at")
+    private Timestamp updated_at;
 
     @Column(name = "content")
     private String content;
+
+
 
     public Integer getId() {
         return id;
@@ -34,20 +44,20 @@ public class Tasks {
         this.id = id;
     }
 
-    public Timestamp getCreate_at() {
-        return create_at;
+    public Timestamp getCreated_at() {
+        return created_at;
     }
 
-    public void setCreate_at(Timestamp create_at) {
-        this.create_at = create_at;
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
     }
 
-    public Timestamp getUpdate_at() {
-        return update_at;
+    public Timestamp getUpdated_at() {
+        return updated_at;
     }
 
-    public void setUpdate_at(Timestamp update_at) {
-        this.update_at = update_at;
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
 
     public String getContent() {
